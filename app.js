7bcfcc10b9997ed55c6b2f6dac52ba42c8244c68
aminelinux <74580181+aminelinux@users.noreply.github.com>
2022-12-11 22:10:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,25 +15,13 @@ const html = `
     <title>Effets divers en Javascript</title>
     <script type="text/javascript">
 function setColor (color) {
-  if (color == 'black') {
-    objet = document.getElementById("para")
-    objet.innerHTML =
-      '<button id="rouge" onclick="setColor(\'red\');">rouge</button>' +
-      '<button id="vert" onclick="setColor(\'green\');">vert</button>' +
-      '<button id="bleu" onclick="setColor(\'blue\');">bleu</button>' +
-      '<button id="blanc" onclick="setColor(\'white\');">blanc</button>' +
-      ' <i>Nouveau contenu</i> ' +
-      '<button id="gris" onclick="setColor(\'gray\');">gris</button>';
-  }
-  else {
-    document.getElementById("body").style.backgroundColor = color;
-  }
+    document.getElementById("body").style.backgroundColor = color; 
   return 0;
 }
     </script>
   </head>
 <body id="body">
-  <p id="para">
+  <p>
     <button id="rouge" onclick="setColor('red');">rouge</button>
     <button id="vert" onclick="setColor('green');">vert</button>
     <button id="bleu" onclick="setColor('blue');">bleu</button>
